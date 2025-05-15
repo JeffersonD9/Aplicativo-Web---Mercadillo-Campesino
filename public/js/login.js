c692fc6a8d33ba62f58d2b1fa1d5d4b88262d6b7
Jefferson <jeffersonm0922@gmail.com>
2025-05-15 08:25:17 -0500

    Add new feature to limit request by IP

--- public/js/login.js
+++ public/js/login.js
@@ -11,43 +11,50 @@ inputs.forEach((input) => {
 });
 
 
-function eliminarError(){
-   mensajeError.classList.toggle("escondido",true);
+function eliminarError() {
+  if (!mensajeError.classList.contains("escondido")) {
+    mensajeError.classList.add("escondido");
+  }
 }
 
-async function iniciarSesion(e){
-    e.preventDefault();
-    let Email = (document.getElementById('email').value).trim();
-    let Password = (document.getElementById('password').value).trim();
-    console.log("ingreso acá")
-    try{
-        const registroInicio = await fetch(
-            "http://localhost:3000/MercadilloBucaramanga/Login",
-            {
-              method: "POST",
-              headers: {
-                "Content-type": "application/json",
-              },
-              body: JSON.stringify({
-                Email,
-                Password
-              }),
-            }
-          );
-            const jsonInicio = await registroInicio.json();
-          
-          if(!registroInicio.ok){
+async function iniciarSesion(e) {
+  e.preventDefault();
+  let Email = (document.getElementById('email').value).trim();
+  let Password = (document.getElementById('password').value).trim();
+  console.log("ingreso acá")
+  try {
+    const registroInicio = await fetch(
+      "http://localhost:3000/MercadilloBucaramanga/Login",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          Email,
+          Password
+        }),
+      }
+    );
 
-            return mensajeError.classList.toggle("escondido",false);
-
-          }
-        
-          window.location.href = jsonInicio.redirect;
-          
-          
-    }catch(e){
-        console.log(e.message);
+    if (!registroInicio.ok) {
+      if (registroInicio.status === 429) {
+        mensajeError.textContent = "Has superado el número de intentos. Intenta de nuevo en 10 minutos.";
+        console.log("Mensaje error 429:", mensajeError.textContent);
+      } else {
+        mensajeError.textContent = "Correo o contraseña incorrectos.";
+      }
+      mensajeError.classList.remove("escondido");
+      return;
     }
+
+    const jsonInicio = await registroInicio.json();
+    window.location.href = jsonInicio.redirect;
+
+
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
-forminciarSesion.addEventListener("submit",(e)=>{iniciarSesion(e)});
+forminciarSesion.addEventListener("submit", (e) => { iniciarSesion(e) });
--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -3,11 +3,12 @@ import { Register } from '../controllers/ControllerRegister.js'
 import { Login, LogOut } from '../controllers/ControllerLogin.js'
 import { Ingresar, IngresarFormRegistroUsuario } from '../controllers/ControllerLogin.js'
 import { EnviarCorreo, FromCambiarPassword, RestablecerPassword, ActualizarPassword } from "../controllers/ControllerAuthSalesman.js";
+import { loginRequestLimiter } from "../MiddleWares/ReqLimiter.js";
 
 
 const router = Router()
 router.get("/Login", Ingresar)
-router.post("/Login", Login)
+router.post("/Login", loginRequestLimiter, Login)
 
 router.post("/LogOut", LogOut)
 
